80fa498468868ecba8abb652834ccc1075a76995
Marfred Barrera <mrbarrera@ucsd.edu>
2025-04-23 09:04:41 -0700

    renaming py files

--- sensor_readers/run_sensors.sh
+++ sensor_readers/run_sensors.sh
@@ -1,9 +1,9 @@
 # Run sample.py in the background
-python c:/Users/remba/UCSD/MAE_Capstone_Project/RPHD_code/sensor_readers/sample.py -t 100 COM6 &
+python c:/Users/remba/UCSD/MAE_Capstone_Project/RPHD_code/sensor_readers/sampleNDI.py -t 100 COM6 &
 SAMPLE_NDI=$!
 
 # Run serial_reader.py in the background
-python c:/Users/remba/UCSD/MAE_Capstone_Project/RPHD_code/sensor_readers/serial_reader.py "$@" &
+python c:/Users/remba/UCSD/MAE_Capstone_Project/RPHD_code/sensor_readers/sampleATI.py "$@" &
 SAMPLE_ATI=$!
 
 # Wait for both processes to finish
