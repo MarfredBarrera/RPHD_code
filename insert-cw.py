73805ef4e80372d13db1cc0ecc76fc359104dd39
JohnnyLiafk <Jol048@ucsd.edu>
2025-05-21 14:03:20 -0700

    Add files via upload
    Updated insert/retract files with limit switch implemented

--- insert-cw.py
+++ insert-cw.py
@@ -8,28 +8,51 @@ from time import sleep
 DIR = 10
 # Step pin from controller
 STEP = 8
+# Limit switch GPIO Pin
+lim1 = 37  # Pin 37 - GPIO 26
+# lim2 = 31 # Pin 31 - GPIO 6
+
+# 0/1 used to signify clockwise or counterclockwise.
+CW = 0
+CCW = 1
 
 # Setup pin layout on PI
 GPIO.setmode(GPIO.BOARD)
 
-
 # Establish Pins in software
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
+GPIO.setup(lim1, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+# GPIO.setup(lim2, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 # Set the direction you want it to spin
 GPIO.output(DIR, CW) 
 
+# Ask for user input for distance to travel
+distance = int(input("Please enter the distance to travel in mm (insertion): "))
+# Convert distance value x into # steps (full step, no microstepping)
+# Step Angle = 1.8deg --> 200 steps/rev | lead srew mitch = 5mm --> 5mm/rev
+n = distance * (200/5) # distance_mm * steps/mm
+n = round(n) # range function only accepts integers
+print("Insertion in progress...")
+
 try:
-	# Run for n steps. This will change based on how you set you controller
-	for x in range(500):
+	# Run for n steps. 
+	for x in range(n):
+		# Limit switch activated = stops linear rail.
+		if GPIO.input(lim1) == GPIO.HIGH:
+			print("Limit switch activated. Stopping...")
+			break
+		#if GPIO.input(lim2 == GPIO.HIGH:
+			#print("Limit switch activated. Stopping...")
+			#break
 		# Set one coil winding to high
 		GPIO.output(STEP,GPIO.HIGH)
 		# Allow it to get there.
-		sleep(.0015) # Dictates how fast stepper motor will run
+		sleep(.001325) # Dictates how fast stepper motor will run
 		# Set coil winding to low
 		GPIO.output(STEP,GPIO.LOW)
-		sleep(.0015) # Dictates how fast stepper motor will run
+		sleep(.001325) # Dictates how fast stepper motor will run
 
 # Once finished clean everything up
 except KeyboardInterrupt:
